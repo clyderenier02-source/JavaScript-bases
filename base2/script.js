902f6c320fc3b117589de31fde3d7780d284983d
Renier Clyde <clyde.renier02gmail.com>
2025-11-25 14:57:42 +0100

    fin base 2

--- base2/script.js
+++ base2/script.js
@@ -60,10 +60,85 @@ for(let i = 1; i <= 10; i++) {
 
 // Exercice 7
 
-let userNumber = parseInt(prompt("Affiche table de multiplication choisie"));
+// let userNumber = parseInt(prompt("Affiche table de multiplication choisie"));
 
-for(let i = 1; i <= 10; i++) {
-    const result = i * userNumber;
+// for(let i = 1; i <= 10; i++) {
+//     const result = i * userNumber;
     
-    console.log(userNumber,"x", i, " = ", result);
+//     console.log(userNumber + "x" + i + " = " + result);
+// }
+
+// Exercice 8
+
+// let result = 0;
+
+// for(let i= 1; i <= 100; i++) {
+//     console.log("calcul en cours", result)
+//     result += i;
+// }
+
+// Exercice 9
+
+// let numberToFind = Math.floor(Math.random() * 10 + 1);
+// finding = true;
+
+// while(finding) {
+// //    devine le nombre
+//     let proposition = parseInt(prompt("Deviner un nombre entre 1 et 10"));
+//     if(numberToFind === proposition) {
+//         alert("Bravo, c’est correct !");
+//         finding = false;
+//     }
+//     else {
+//         alert("Essaye encore");
+//     }
+// }
+
+// Exercice 10
+
+for(let i = 0; i <= 50; i+=2) {
+    console.log(i);
 }
+
+// Exercice 11
+
+for(let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+console.log("C’est terminé !")
+
+// Exercice 12
+
+// let operation = prompt("Entrer une opération");
+// let number1 = parseInt(prompt("Entrer le 1 nombres"));
+// let number2 = parseInt(prompt("Entrer le 2 nombres"));
+
+// if(operation === "addition") {
+//     let result = number1 + number2;
+//     console.log(`${number1} + ${number2} = ${result}`);
+// } 
+// else if(operation === "soustraction") {
+//     let result = number1 - number2;
+//     console.log(`${number1} - ${number2} = ${result}`);
+// } 
+// else if(operation === "multiplication") {
+//     let result = number1 * number2;
+//     console.log(`${number1} * ${number2} = ${result}`);
+// } 
+// else if(operation === "division") {
+//     let result = number1 / number2;
+//     console.log(`${number1} / ${number2} = ${result}`);
+// }
+
+// Exerxcice 13
+
+const table = [12, 45, 3, 22, 34];
+let max = table[0];
+
+for(let i = 0; i < table.length; i++) {
+    if(table[i] > max) {
+        max = table[i];
+    }
+}
+
+console.log(`Le plus grand nombre est ${max}`)
